7fc423bde20c30db60cdf00d6821ad1ec84203b5
Ermal Bujupaj <ermal.bj@gmail.com>
2021-01-02 13:30:22 +0100

    Open CreateNewHypedEvent from upcoming view navigation view.

--- HypedList/UpcomingView.swift
+++ HypedList/UpcomingView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct UpcomingView: View {
+    
+    @State var showingCreateView = false
+    
     var body: some View {
         Text("Upcoming!")
             .navigationTitle("Upcoming")
             .navigationBarItems(trailing:
-                                    Button(action: {}) {
+                                    Button(action: {
+                                        showingCreateView = true
+                                    }) {
                                         Image(systemName: "calendar.badge.plus")
                                             .font(.title)
+                                    }
+                                    .sheet(isPresented: $showingCreateView) {
+                                        CreateHypedEventView()
                                     }
             )
     }
