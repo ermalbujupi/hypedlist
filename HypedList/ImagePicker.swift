a1c1b47712ce4a74ec6d29d4656483984bde8e70
Ermal Bujupaj <ermal.bj@gmail.com>
2021-01-02 23:42:30 +0100

    Add ImagePicker controller.

--- HypedList/ImagePicker.swift
+++ HypedList/ImagePicker.swift
@@ -5,4 +5,37 @@
 //  Created by Ermal Bujupaj on 2.1.21.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+        
+        let parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                image.pngData()
+            }
+        }
+    }
+}
