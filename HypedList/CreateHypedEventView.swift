b308e0ab53c840a3bd15487c40629f0edef1b0ec
Ermal Bujupaj <ermal.bj@gmail.com>
2021-01-02 23:42:17 +0100

    Add CreateHypedEventView.

--- HypedList/CreateHypedEventView.swift
+++ HypedList/CreateHypedEventView.swift
@@ -10,25 +10,45 @@ import SwiftUI
 struct CreateHypedEventView: View {
     
     @StateObject var hypedEvent = HypedEvent()
+    @State var showTime = false
+    @State var showImagePicker = false
     
     var body: some View {
         Form {
             Section {
+                FormLabelView(title: "Title", iconName: "keyboard", backgroundColor: .orange)
                 TextField("Family Vacation", text: $hypedEvent.title)
                     .autocapitalization(.words)
             }
             
             Section {
-                DatePicker("Date", selection: $hypedEvent.date, displayedComponents: [.date, .hourAndMinute])
+                FormLabelView(title: "Date", iconName: "calendar", backgroundColor: .blue)
+                DatePicker("Date", selection: $hypedEvent.date, displayedComponents: showTime ? [.date, .hourAndMinute] : .date)
                     .datePickerStyle(GraphicalDatePickerStyle())
+                Toggle(isOn: $showTime) {
+                    FormLabelView(title: "Time", iconName: "clock.fill", backgroundColor: .blue)
+                }
             }
             
             Section {
-                ColorPicker("Color", selection: $hypedEvent.color)
+                Button(action: {
+                    showImagePicker = true
+                }) {
+                    Text("Pick image")
+                }.sheet(isPresented: $showImagePicker, content: {
+                    ImagePicker()
+                })
+            }
+            
+            Section {
+                ColorPicker(selection: $hypedEvent.color) {
+                    FormLabelView(title: "Color Picker", iconName: "eyedropper", backgroundColor: .yellow)
+                }
             }
                 
             Section {
-                TextField("Website", text: $hypedEvent.url)
+                FormLabelView(title: "URL Link", iconName: "link", backgroundColor: .orange)
+                TextField("website", text: $hypedEvent.url)
                     .keyboardType(.URL)
                     .autocapitalization(.none)
             }
