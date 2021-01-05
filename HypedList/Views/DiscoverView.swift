6e5bf0c82ad00f136d97231a3cd30c2a65d394df
Ermal Bujupaj <ermal.bj@gmail.com>
2021-01-05 18:55:01 +0100

    Add DiscoveryView.

---
+++ HypedList/Views/DiscoverView.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverView.swift
+//  HypedList
+//
+//  Created by Ermal Bujupaj on 5.1.21.
+//
+
+import SwiftUI
+
+struct DiscoverView: View {
+    
+    @ObservedObject var data = DataController.shared
+    
+    var body: some View {
+        HypedEventListView(hypedEvents: data.discoverHypedEvents.sorted { $0.date < $1.date }, noEventsText: "Loading some awesome stuff for you 😜")
+            .navigationTitle("Discover")
+            .navigationBarItems(trailing: Button(action: {
+                data.getDiscoverEvents()
+            }) {
+                Image(systemName: "arrow.clockwise")
+                    .font(.title)
+            })
+    }
+}
+
+struct DiscoverView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiscoverView()
+    }
+}
