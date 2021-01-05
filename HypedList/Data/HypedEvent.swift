49223d0c8195f93dd12276542b490ecf687568f9
Ermal Bujupaj <ermal.bj@gmail.com>
2021-01-05 14:12:04 +0100

    Conform HypedEvent model to Codable.

--- HypedList/Data/HypedEvent.swift
+++ HypedList/Data/HypedEvent.swift
@@ -8,14 +8,48 @@
 import Foundation
 import SwiftUI
 import SwiftDate
+import UIColor_Hex_Swift
 
-class HypedEvent: ObservableObject, Identifiable{
+class HypedEvent: ObservableObject, Identifiable, Codable {
     var id = UUID().uuidString
     var date = Date()
     var title = ""
     var url = ""
     var color = Color.purple
     @Published var imageData: Data?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case date
+        case title
+        case url
+        case color
+        case imageData
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: CodingKeys.id)
+        try container.encode(date, forKey: CodingKeys.date)
+        try container.encode(title, forKey: CodingKeys.title)
+        try container.encode(url, forKey: CodingKeys.url)
+        try container.encode(UIColor(color).hexString(), forKey: CodingKeys.color)
+        try container.encode(imageData, forKey: CodingKeys.imageData)
+        
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        id = try values.decode(String.self, forKey: .id)
+        date = try values.decode(Date.self, forKey: .date)
+        title = try values.decode(String.self, forKey: .title)
+        url = try values.decode(String.self, forKey: .url)
+        let colorHex = try values.decode(String.self, forKey: .color)
+        color = Color(UIColor(colorHex))
+        imageData = try? values.decode(Data.self, forKey: .imageData)
+    }
+    
+    init() {}
     
     func image() -> Image? {
         if let data = imageData, let image = UIImage(data: data) {
